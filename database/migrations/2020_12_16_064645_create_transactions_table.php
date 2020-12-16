05299e4467a7d95f48374c46bee7eed462150e9e
akuikialie <aku.4l13@gmail.com>
2020-12-16 19:27:29 +0700

    add table

---
+++ database/migrations/2020_12_16_064645_create_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('journal_id')
+                ->nullable()
+                ->constrained();
+            $table->date('date_at');
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->enum('type', ['debit', 'credit']);
+            $table->float('amount', 8, 0);
+            $table->enum('status', ['draft', 'process', 'done']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
