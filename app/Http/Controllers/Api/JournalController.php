f3a5049777913cf0d1a3b77e2a69bddd5d1ddd19
akuikialie <aku.4l13@gmail.com>
2020-12-16 19:27:51 +0700

    add new controller

---
+++ app/Http/Controllers/Api/JournalController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+use App\Models\Journal;
+
+use App\Http\Controllers\Controller;
+
+class JournalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $journal = new Journal();
+        return response()->json($journal->get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Journal::create($request->all());
+
+        return response()->json([
+            'message' => 'Success'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Journal  $journal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Journal $journal)
+    {
+        return response()->json($journal->first());
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Journal  $journal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Journal $journal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Journal  $journal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Journal $journal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Journal  $journal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Journal $journal)
+    {
+        //
+    }
+}
