086455691cc293d48e15df89209147c6a364f176
akuikialie <aku.4l13@gmail.com>
2020-12-25 13:30:16 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('default');
 });
 
 Route::prefix('transaction')->name('transaction.')->group(function () {
