b2cd7a73565642169ff87c8aef6f3c8541dcc769
akuikialie <aku.4l13@gmail.com>
2023-12-12 13:47:45 +0700

    fix send test wa

--- app/Http/Controllers/External/WhatsappGateway/SendTestController.php
+++ app/Http/Controllers/External/WhatsappGateway/SendTestController.php
@@ -18,61 +18,68 @@ use DateTimeZone;
 
 class SendTestController extends Controller
 {
-     public function send()
+    public function send($phone, $message)
     {
+        // var_dump($request->all());
+        // exit;
         $client = new Client([
             'verify' => false
         ]);
         // $target[] = '085730432092|Ali|Superadmin';
         // $target[] = '081917787195|Umi Ayam Aldi|Pasar';
         // $target[] = '081230788990|Abah Yusuf Ayam|Pasar';
-        $target[] = '087840180475|Anak Abah Kholili|Pasar';
+        // $target[] = '087840180475|Anak Abah Kholili|Pasar';
         // $target[] = '085733171373|Tifani|Finance';
         // $target[] = '081917787195';
 
-var_dump($target);
+        // var_dump($target);
 
-    $dateTime = '2023-06-30 12:40:00'; 
-    $tz_from = 'Asia/Jakarta'; 
-    $newDateTime = new DateTime($dateTime, new DateTimeZone($tz_from)); 
+        // exit;
 
-    $dateTimeUTC = $newDateTime->format("Y-m-d H:i:s");
-    // var_dump($dateTimeUTC);
-    $dateTimeUTCInteger = strtotime($dateTimeUTC);
-    // var_dump($dateTimeUTCInteger);
+        // $dateTime = '2023-06-30 12:40:00';
+        // $tz_from = 'Asia/Jakarta';
+        // $newDateTime = new DateTime($dateTime, new DateTimeZone($tz_from));
 
-    // $newDateTime->setTimezone(new DateTimeZone("UTC")); 
-    // $dateTimeUTC = $newDateTime->format("Y-m-d H:i:s");
-    // var_dump($dateTimeUTC);
-    // $dateTimeUTCInteger = strtotime($dateTimeUTC);
-// var_dump($dateTimeUTCInteger);
-        
-        $target = implode(',', $target);
-        $response = $client->post('https://api.fonnte.com/send',
+        // $dateTimeUTC = $newDateTime->format("Y-m-d H:i:s");
+        // // var_dump($dateTimeUTC);
+        // $dateTimeUTCInteger = strtotime($dateTimeUTC);
+        // var_dump($dateTimeUTCInteger);
+
+        // $newDateTime->setTimezone(new DateTimeZone("UTC"));
+        // $dateTimeUTC = $newDateTime->format("Y-m-d H:i:s");
+        // var_dump($dateTimeUTC);
+        // $dateTimeUTCInteger = strtotime($dateTimeUTC);
+        // var_dump($dateTimeUTCInteger);
+
+        // $target = implode(',', $target);
+        $response = $client->post(
+            'https://api.fonnte.com/send',
             [
                 'headers' => [
                     // 'Authorization' => env('FONNTE_TOKEN'),
-                    // 'Authorization' => 'nAhCYxuXJx2VrZJAaSt7', // WA IPHONE
-                    'Authorization' => 'GdJ14EeFJ7W89i57j81b', // WA BISNIS
+                    'Authorization' => 'nAhCYxuXJx2VrZJAaSt7', // WA IPHONE
+                    // 'Authorization' => 'GdJ14EeFJ7W89i57j81b', // WA BISNIS
                 ],
                 'form_params' =>
-                    [
-                        // 'target' => '085730432092',
-                        // 'message' => 'via GuzzleHttp',
-                        'target' => $target,
-                        'message' => 'Assalamualaikum, ayam 2',
-                        // 'schedule' => '1687329757',
-                        'schedule' => $dateTimeUTCInteger,
-                        // 'message' => 'Assalamualaikum, {name} ({var1}). Coba send WhatsappGateway : '. Carbon::now()->format('Y-m-d H:i:s'),
-                        // 'message' => 'Coba send WhatsappGateway : '. Carbon::now()->format('Y-m-d H:i:s'),
-                        // 'url' => 'https://md.fonnte.com/images/wa-logo.png',
-                        // 'url' => 'https://cdn-indraco.storage.googleapis.com/icons/hris.jpg',
-                        // 'url' => 'https://filesamples.com/samples/document/txt/sample3.txt',
-                        // 'location' => '-7.983908, 112.621391',
-                        // 'location' => '-7.2929482,112.6971682,17z', // kantor indraco
-                        // 'message' => 'test message to {name} as {var1}',
-                        'typing' => true,
-                    ]
+                [
+                    // 'target' => '085730432092',
+                    // 'message' => 'via GuzzleHttp',
+                    // 'target' => $target,
+                    'target' => $phone,
+                    'message' => $message,
+                    // 'message' => 'Assalamualaikum, ayam 2',
+                    // 'schedule' => '1687329757',
+                    // 'schedule' => $dateTimeUTCInteger,
+                    // 'message' => 'Assalamualaikum, {name} ({var1}). Coba send WhatsappGateway : '. Carbon::now()->format('Y-m-d H:i:s'),
+                    // 'message' => 'Coba send WhatsappGateway : '. Carbon::now()->format('Y-m-d H:i:s'),
+                    // 'url' => 'https://md.fonnte.com/images/wa-logo.png',
+                    // 'url' => 'https://cdn-indraco.storage.googleapis.com/icons/hris.jpg',
+                    // 'url' => 'https://filesamples.com/samples/document/txt/sample3.txt',
+                    // 'location' => '-7.983908, 112.621391',
+                    // 'location' => '-7.2929482,112.6971682,17z', // kantor indraco
+                    // 'message' => 'test message to {name} as {var1}',
+                    'typing' => true,
+                ]
             ]
         );
         $body = json_decode((string)$response->getBody());
@@ -81,12 +88,26 @@ var_dump($target);
         return $body->success;
     }
 
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
         try {
-            $sendRequestApi = $this->send();
-            if ($sendRequestApi->status) {
+            if (!$request->has('phone')) {
+                return response()->json([
+                    'code' => 400,
+                    'status' => false,
+                    'message' => 'No Phone',
+                ], 200);
+            }
+            if (!$request->has('message')) {
                  return response()->json([
+                    'code' => 400,
+                    'status' => false,
+                    'message' => 'No Message',
+                ], 200);
+            }
+            $sendRequestApi = $this->send($request->phone, $request->message);
+            if ($sendRequestApi->status) {
+                return response()->json([
                     'code' => 200,
                     'status' => true,
                     'message' => 'Successfull Send',
@@ -114,58 +135,56 @@ var_dump($target);
                 'message' => $ex->getMessage(),
             ], 400);
         }
-        exit;
+    //     exit;
 
-        // $curl = curl_init();
-        // curl_setopt_array($curl, array(
-        //   CURLOPT_URL => 'https://api.fonnte.com/send',
-        //   CURLOPT_RETURNTRANSFER => true,
-        //   CURLOPT_ENCODING => '',
-        //   CURLOPT_MAXREDIRS => 10,
-        //   CURLOPT_TIMEOUT => 0,
-        //   CURLOPT_FOLLOWLOCATION => true,
-        //   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        //   CURLOPT_CUSTOMREQUEST => 'POST',
-        //   CURLOPT_POSTFIELDS => array(
-        //     'target' => '085730432092',
-        //     'message' => 'test dari postman laravel'
-        //     /*
-        //     'target' => $target,
-        //     'message' => 'test message to {name} as {var1}',
-        //     'url' => 'https://md.fonnte.com/images/wa-logo.png',
-        //     'filename' => 'filename',
-        //     'schedule' => '0',
-        //     'typing' => false,
-        //     'delay' => '2',
-        //     'countryCode' => '62',
-        //     'location' => '-7.983908, 112.621391',
-        //     'buttonJSON' => '{"message":"fonnte button message","footer":"fonnte footer message","buttons":[{"id":"mybutton1","message":"hello fonnte"},{"id":"mybutton2","message":"fonnte pricing"},{"id":"mybutton3","message":"tutorial fonnte"}]}',
-        //     'templateJSON' => '{"message":"fonnte template message","footer":"fonnte footer message","buttons":[{"message":"fonnte","url":"https://fonnte.com"},{"message":"call me","tel":"6282227097005"},{"id":"mybutton1","message":"hello fonnte"}]}',
-        //     'listJSON' => '{"message":"fonnte list message","footer":"fonnte footer message","buttonTitle":"fonnte\'s packages","title":"fonnte title","buttons":[{"title":"text only","list":[{"message":"regular","footer":"10k messsages/month","id":"list-1"},{"message":"regular pro","footer":"25k messsages/month","id":"list-2"},{"message":"master","footer":"unlimited messsages/month","id":"list-3"}]},{"title":"all feature","list":[{"message":"super","footer":"10k messsages/month","id":"list-4"},{"message":"advanced","footer":"25k messsages/month","id":"list-5"},{"message":"ultra","footer":"unlimited messsages/month","id":"list-6"}]}]}'
-        //     */
-        //     ),
-        //   CURLOPT_HTTPHEADER => array(
-        //     'Authorization: nAhCYxuXJx2VrZJAaSt7'
-        //   ),
-        // ));
+    //     // $curl = curl_init();
+    //     // curl_setopt_array($curl, array(
+    //     //   CURLOPT_URL => 'https://api.fonnte.com/send',
+    //     //   CURLOPT_RETURNTRANSFER => true,
+    //     //   CURLOPT_ENCODING => '',
+    //     //   CURLOPT_MAXREDIRS => 10,
+    //     //   CURLOPT_TIMEOUT => 0,
+    //     //   CURLOPT_FOLLOWLOCATION => true,
+    //     //   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    //     //   CURLOPT_CUSTOMREQUEST => 'POST',
+    //     //   CURLOPT_POSTFIELDS => array(
+    //     //     'target' => '085730432092',
+    //     //     'message' => 'test dari postman laravel'
+    //     //     /*
+    //     //     'target' => $target,
+    //     //     'message' => 'test message to {name} as {var1}',
+    //     //     'url' => 'https://md.fonnte.com/images/wa-logo.png',
+    //     //     'filename' => 'filename',
+    //     //     'schedule' => '0',
+    //     //     'typing' => false,
+    //     //     'delay' => '2',
+    //     //     'countryCode' => '62',
+    //     //     'location' => '-7.983908, 112.621391',
+    //     //     'buttonJSON' => '{"message":"fonnte button message","footer":"fonnte footer message","buttons":[{"id":"mybutton1","message":"hello fonnte"},{"id":"mybutton2","message":"fonnte pricing"},{"id":"mybutton3","message":"tutorial fonnte"}]}',
+    //     //     'templateJSON' => '{"message":"fonnte template message","footer":"fonnte footer message","buttons":[{"message":"fonnte","url":"https://fonnte.com"},{"message":"call me","tel":"6282227097005"},{"id":"mybutton1","message":"hello fonnte"}]}',
+    //     //     'listJSON' => '{"message":"fonnte list message","footer":"fonnte footer message","buttonTitle":"fonnte\'s packages","title":"fonnte title","buttons":[{"title":"text only","list":[{"message":"regular","footer":"10k messsages/month","id":"list-1"},{"message":"regular pro","footer":"25k messsages/month","id":"list-2"},{"message":"master","footer":"unlimited messsages/month","id":"list-3"}]},{"title":"all feature","list":[{"message":"super","footer":"10k messsages/month","id":"list-4"},{"message":"advanced","footer":"25k messsages/month","id":"list-5"},{"message":"ultra","footer":"unlimited messsages/month","id":"list-6"}]}]}'
+    //     //     */
+    //     //     ),
+    //     //   CURLOPT_HTTPHEADER => array(
+    //     //     'Authorization: nAhCYxuXJx2VrZJAaSt7'
+    //     //   ),
+    //     // ));
 
-        // $response = curl_exec($curl);
+    //     // $response = curl_exec($curl);
 
-        // curl_close($curl);
+    //     // curl_close($curl);
 
-        return response()->json([
-            'App Name'          => env('APP_NAME'),
-            'App Prefix'        => 'API - EXTERNAL - DEFAULT',
-            'App Timezone'      => env('APP_TIMEZONE'),
-            'Engine'            => app()->version(),
-            'IP_address'        => $_SERVER['REMOTE_ADDR'],
-            'Fonnte'            => env('FONNTE_TOKEN'),
-            'PHP'               => phpversion() . '-' . php_sapi_name(),
-            'OS'                => php_uname(),
-            'OS Time'           => strftime('%c'),
-            'response'          => $response,
-        ], 200, [], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    //     return response()->json([
+    //         'App Name'          => env('APP_NAME'),
+    //         'App Prefix'        => 'API - EXTERNAL - DEFAULT',
+    //         'App Timezone'      => env('APP_TIMEZONE'),
+    //         'Engine'            => app()->version(),
+    //         'IP_address'        => $_SERVER['REMOTE_ADDR'],
+    //         'Fonnte'            => env('FONNTE_TOKEN'),
+    //         'PHP'               => phpversion() . '-' . php_sapi_name(),
+    //         'OS'                => php_uname(),
+    //         'OS Time'           => strftime('%c'),
+    //         'response'          => $response,
+    //     ], 200, [], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
     }
-
-  
 }
